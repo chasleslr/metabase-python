5e66abfd3745b9db39b2bd21f7589b236392117a
Charles Larivier <charles@dribbble.com>
2021-10-27 00:13:11 -0400

    feat: add User

---
+++ metabase/user.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from typing import List
+
+from metabase.resource import Resource
+
+
+@dataclass
+class User(Resource):
+    ENDPOINT = "/api/user"
+
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    password: str = None
+    groups_ids: List[int] = None
+    login_attributes: dict = None
+
+    is_superuser: bool = None
+    locale: str = None
+
+    @classmethod
+    def create(cls, first_name: str, last_name: str, email: str, **kwargs) -> "User":
+        response = cls.connection().post(
+            cls.ENDPOINT,
+            json={
+                "first_name": first_name,
+                "last_name": last_name,
+                "email": email,
+                **kwargs
+            }
+        )
+        return cls.from_dict(response.json())
+
+    def send_invite(self):
+        self.connection().put(self.ENDPOINT + f"/{self.id}" + "/send_invite")
+
+    def reactivate(self):
+        self.connection().put(self.ENDPOINT + f"/{self.id}" + "/reactivate")
