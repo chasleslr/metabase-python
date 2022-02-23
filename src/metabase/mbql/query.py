36cb4863a97aff6d6c63ea991d5ad0f5d44b9d90
Charles Larivier <charles@dribbble.com>
2022-02-22 23:27:58 -0500

    feat: add query.py
    Signed-off-by: Charles Larivier <charles@dribbble.com>

--- src/metabase/mbql/query.py
+++ src/metabase/mbql/query.py
@@ -1,21 +1,40 @@
 from dataclasses import dataclass, field
-from typing import List, Union
+from typing import List
 
 from metabase.mbql.aggregations import Aggregation
+from metabase.mbql.filter import Filter
 from metabase.mbql.groupby import GroupBy
 
 
 @dataclass
 class Query:
     table_id: int
-    aggregations: List[Union[Aggregation, Metric]]
+    aggregations: List[Aggregation]
     group_by: List[GroupBy] = field(default_factory=list)
     filters: List[Filter] = field(default_factory=list)
 
     def compile(self):
         return {
             "source-table": self.table_id,
-            "aggregation": [agg.compile() for agg in self.aggregations],
-            "breakout": [],
-            "filter": [],
+            "aggregation": self._aggregations,
+            "breakout": self._group_by,
+            "filter": self._filters,
         }
+
+    @property
+    def _aggregations(self):
+        return [aggregation.compile() for aggregation in self.aggregations]
+
+    @property
+    def _group_by(self):
+        return [group.compile() for group in self.group_by]
+
+    @property
+    def _filters(self):
+        if len(self.filters) == 0:
+            return self.filters
+
+        if len(self.filters) == 1:
+            return self.filters[0].compile()
+
+        return ["and"] + [filt.compile() for filt in self.filters]
