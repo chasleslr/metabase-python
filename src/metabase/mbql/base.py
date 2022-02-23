32bace6cf81a7345e9a8b5327626daee38cb97e0
Charles Larivier <charles@dribbble.com>
2022-02-22 21:50:52 -0500

    feat: add Mbql
    Signed-off-by: Charles Larivier <charles@dribbble.com>

--- src/metabase/mbql/base.py
+++ src/metabase/mbql/base.py
@@ -1,18 +1,17 @@
 from typing import List
 
 
+class Option:
+    pass
+
+
 class Mbql:
-    def compile(self):
-        raise NotImplementedError()
-
-    def __repr__(self):
-        return str(self.compile())
-
-
-class Field(Mbql):
-    def __init__(self, id: int, option=None):
+    def __init__(self, id: int, option: Option = None):
         self.id = id
         self.option = option
 
     def compile(self) -> List:
         return ["field", self.id, self.option]
+
+    def __repr__(self):
+        return str(self.compile())
