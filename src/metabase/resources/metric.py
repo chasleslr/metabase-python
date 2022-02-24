67c65a7915163cc1180ba9f9e5aae8739b2e0367
Charles Lariviere <charles@dribbble.com>
2022-02-24 00:25:53 -0500

    fix: circular import
    Signed-off-by: Charles Lariviere <charles@dribbble.com>

--- src/metabase/resources/metric.py
+++ src/metabase/resources/metric.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 from datetime import datetime
 from typing import List
 
-from metabase import Metabase
+from metabase.metabase import Metabase
 from metabase.missing import MISSING
 from metabase.resource import CreateResource, GetResource, ListResource, UpdateResource
 
